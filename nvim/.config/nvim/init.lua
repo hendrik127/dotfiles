7ca49b19a3bdbe7243b15c00634ce7f6e1e9a21d
Hendrik Parik <hendrik.parik@gmail.com>
2025-11-26 13:59:03 +0200

    new nvim config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,45 @@
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+vim.o.swapfile = false
+vim.g.mapleader =" " 
+
+
+vim.cmd("colorscheme habamax")
+
+vim.pack.add({
+      { src = 'https://github.com/neovim/nvim-lspconfig' },
+
+      { src = "https://github.com/nvim-lua/plenary.nvim" },
+      { src = 'https://github.com/nvim-telescope/telescope.nvim' },
+})
+
+
+vim.lsp.config('ruff', {
+  init_options = {
+    settings = {
+      configurationPreference = "editorFirst",
+      -- configurationPreference = "filesystemFirst",
+      -- codeAction = {
+      --   fixViolation = {
+      --     enable = true
+      --   }
+      -- }
+    }
+  }
+})
+
+
+vim.lsp.enable('ruff')
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
