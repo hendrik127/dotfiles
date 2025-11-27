6ea2bef290d1c55c6697c70fa05914c83738256c
Hendrik Parik <hendrik.parik@gmail.com>
2025-11-27 16:37:52 +0200

    update

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,17 +6,40 @@ vim.o.shiftwidth = 4
 vim.o.expandtab = true
 
 vim.o.swapfile = false
-vim.g.mapleader =" " 
+vim.o.clipboard = "unnamedplus"
+vim.g.mapleader = " "
+vim.o.undofile = true
 
+vim.g.netrw_winsize = 20
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
 
 
 vim.cmd("colorscheme habamax")
 
 vim.pack.add({
-      { src = 'https://github.com/neovim/nvim-lspconfig' },
+    { src = 'https://github.com/neovim/nvim-lspconfig' },
 
-      { src = "https://github.com/nvim-lua/plenary.nvim" },
-      { src = 'https://github.com/nvim-telescope/telescope.nvim' },
+    { src = "https://github.com/nvim-lua/plenary.nvim" },
+    { src = 'https://github.com/nvim-telescope/telescope.nvim' },
+
+    { src = 'https://github.com/mason-org/mason.nvim' },
+    { src = 'https://github.com/mason-org/mason-lspconfig.nvim' },
+    { src = 'https://github.com/stevearc/conform.nvim' }
+})
+
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+require("conform").setup({
+    formatters_by_ft = {
+        python = { "ruff_format" },
+    },
+    format_on_save = {
+        timeout_ms = 2000,
+        lsp_fallback = true,
+    },
 })
 
 
